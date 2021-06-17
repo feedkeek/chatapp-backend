ac36c41f4d08917cbbc9cb9c4e9a6421e3a64423
Jafar Hasanov <feedkeek@gmail.com>
2021-06-17 13:04:13 +0400

    Auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import UserModel, { IUser } from "../models/User";
+
+const auth = async (req: any, res: any, next: any) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const data: any = jwt.verify(token, process.env.JWT_KEY);
+        const user: IUser = await UserModel.findOne({ _id: data._id, 'tokens.token': token });
+        if (!user) {
+            throw new Error("No such user");
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: "Not authorized to access this resource" });
+    }
+}
+
+export default auth;
